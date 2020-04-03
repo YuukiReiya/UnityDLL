846955399f79af6dcdf0acf4e41a7a270e2d59d9
Yuuki Bamba <yuukiBamba@gmail.com>
2020-04-03 18:52:46 +0900

    拡張クラスの追加。
    SingletonMonoBehaviour追加
    UnityAPIExoantions追加(GameObject拡張FindTryGetComponent)

---
+++ MyUnityDLL/MyUnityDLL/SingletonMonoBehaviour.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+namespace Util
+{
+    public class SingletonMonoBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        //  static instance
+        private static T instance = null;
+
+        //  getter
+        public static T Instance
+        {
+            get
+            {
+                //  null check!
+                if (instance == null)
+                {
+                    //  find
+                    instance = (T)FindObjectOfType(typeof(T));
+
+                    //  not found!
+                    if (instance == null)
+                    {
+                        Debug.LogError("<color=red>" + typeof(T) + "</color>" + " is nothing");
+                    }
+                }
+                return instance;
+            }
+        }
+
+        /// <summary>
+        /// Awake
+        /// </summary>
+        protected virtual void Awake()
+        {
+            //  null check!
+            if (instance == null)
+            {
+                instance = this as T;
+            }
+
+            //  static check!
+            if (instance != this)
+            {
+                Destroy(this.gameObject);
+                return;
+            }
+        }
+    }
+}
---
+++ MyUnityDLL/MyUnityDLL/UnityAPIExpantions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Util
+{
+    public static class UnityAPIExpantions
+    {
+        /// <summary>
+        /// Find + TryGetComponent
+        /// </summary>
+        /// <param name="name">検索するオブジェクトの名前</param>
+        /// <param name="component">取得するコンポーネントの参照</param>
+        /// <returns>成功判定</returns>
+        public static bool FindTryGetComponent<T>(this GameObject self, string name, out T component) where T : Component
+        {
+            component = null;
+            var obj = GameObject.Find(name);
+            if (obj == null)
+            {
+                Debug.LogError("No object named " + "<color=red>\"" + name + "\"</color>" + " was found.", self);
+                return false;
+            }
+            if (!obj.TryGetComponent(out component))
+            {
+                Debug.LogError("Failed to get component named " + "<color=red>\"" + typeof(T) + "\"</color>.", obj);
+                return false;
+            }
+            return true;
+        }
+    }
+}
