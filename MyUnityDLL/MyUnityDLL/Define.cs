9f4ecf23e377ba3b6e895ff98ce3685f5ffc642b
Yuuki Bamba <yuukiBamba@gmail.com>
2020-04-04 14:30:53 +0900

    定数宣言用のスクリプトを追加。

---
+++ MyUnityDLL/MyUnityDLL/Define.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace Common
+{
+    /// <summary>
+    /// 定数宣言
+    /// </summary>
+    public class Define
+    {
+        private Define() { }
+
+
+        public const float MAX_ALPHA = 1.0f;
+        public const float MIN_ALPHA = 0.0f;
+    }
+}
